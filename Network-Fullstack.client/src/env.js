4895b49840f6c8f376b90fa9023721110b4a7e31
Holli Davis <hollidavis19@gmail.com>
2021-09-17 10:43:36 -0600

    updated env

--- Network-Fullstack.client/src/env.js
+++ Network-Fullstack.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'https://bcw-sandbox.herokuapp.com/' : ''
+export const baseURL = dev ? 'http://localhost:3000' : ''
 export const domain = 'holli-codeworks.us.auth0.com'
 export const clientId = 'WOJPmg4INYdBncBTcsNC39o4sOap0MoP'
 export const audience = 'https://network.com'
