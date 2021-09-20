8579f3eaa0c81c12a090c97ada06ef1fb67f5e34
Holli Davis <hollidavis19@gmail.com>
2021-09-20 11:55:19 -0600

    building

--- Network-Fullstack.client/src/components/Login.vue
+++ Network-Fullstack.client/src/components/Login.vue
@@ -1,28 +1,30 @@
 <template>
   <!-- Profile Img -->
   <div class="row justify-content-center">
-    <img class="profile-img pointer py-4" :src="account.picture" :alt="account.name" @click.stop="getProfileById">
+    <img class="profile-img rounded-pill pointer my-4" :src="account.picture" :alt="account.name" @click.stop="getProfileById">
   </div>
   <!-- Name & Class -->
   <div class="row flex-column">
     <div>
-      <h4>{{ account.name }}</h4>
-      <p class="m-0" v-if="account.class">
+      <h2 class="text-center m-0">
+        {{ account.name }}
+      </h2>
+      <h3 class="text-center mb-3" v-if="account.class">
         <span class="p-2 bg-light round-border fa fa-user-graduate fa-lg" aria-hidden="true" v-if="account.graduated == true"></span>
         <span class="p-2 bg-light round-border fa fa-user fa-lg" aria-hidden="true" v-else></span>
         {{ account.class }}
-      </p>
+      </h3>
     </div>
     <!-- Links -->
     <div>
       <h5 v-if="account.github" class="lead pointer" :href="account.github">
-        <span class="mr-2 fab fa-lg fa-github"></span><b> GitHub</b>
+        <span class="me-2 ms-1 fab fa-lg fa-github"></span><b> GitHub</b>
       </h5>
       <h5 v-if="account.linkedin" class="lead pointer" :href="account.linkedin">
-        <span class="mr-2 fab fa-lg fa-linkedin"></span><b> LinkedIn</b>
+        <span class="me-2 ms-1 fab fa-lg fa-linkedin"></span><b> LinkedIn</b>
       </h5>
       <h5 v-if="account.resume" class="lead pointer" :href="account.resume">
-        <span class="mr-3 fa fa-lg fa-file-alt"></span><b>Resume</b>
+        <span class="me-3 ms-1 fa fa-lg fa-file-alt"></span><b>Resume</b>
       </h5>
     </div>
   </div>
@@ -53,14 +55,9 @@ export default {
 
 <style lang="scss" scoped>
 .profile-img{
-  border-radius: 50%;
-  height: 75%;
-  width: 75%;
+  height: 40vh;
+  width: 40vh;
   object-fit: cover;
   object-position: center;
-}
-.spill{
-margin-right: 20%;
-margin-top: -25%
 }
 </style>
